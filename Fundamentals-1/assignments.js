1c0e9d710d993910ec5f880b9db6ae6fdd1583a3
ikeraguero <ikerpires407@gmail.com>
2023-12-16 12:23:05 -0300

    Add Assignment 4

--- Fundamentals-1/assignments.js
+++ Fundamentals-1/assignments.js
@@ -5,7 +5,7 @@ assign their values according to your own country (population in millions)
 
 const country = 'Brazil';
 const continent = 'South America';
-let population = '214 million';
+let population = 214000000;
 
 /* LECTURE: Data Types
 1. Declare a variable called 'isIsland' and set its value according to your
@@ -26,7 +26,26 @@ change, and which might change?). Then, change these variables to const.
 
 language = "Portuguese";
 
-console.log(isIsland);
-console.log(population);
-console.log(country);
-console.log(language)
+/* LECTURE: Basic Operators
+1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+2. Increase the population of your country by 1 and log the result to the console
+3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese' */
+
+const countryHalfPopulation = population / 2;
+const countryPopulationIncreased = population + 1;
+const countryComparedToFinland = population > 6000000;
+const countryComparedToAverage = population > 33000000;
+const description = country + " " + "is in " + continent + ", and its " + population + " people sepak " + language;
+
+console.log(countryHalfPopulation);
+console.log(countryPopulationIncreased);
+console.log(countryComparedToFinland);
+console.log(countryComparedToAverage)
+console.log(description);
