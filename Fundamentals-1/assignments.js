ff2318659376f253c23b3ac6461a257c83c39cf9
ikeraguero <ikerpires407@gmail.com>
2023-12-16 10:36:28 -0300

    Add Assignments File

---
+++ Fundamentals-1/assignments.js
@@ -0,0 +1,12 @@
+// ASSIGNMENT 1
+// Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console 
+
+let country = 'Brazil';
+let continent = 'South America';
+let population = '214 million';
+
+console.log(country);
+console.log(continent);
+console.log(population)
---
+++ Fundamentals-1/script.js
@@ -0,0 +1,5 @@
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
+
+let math = 10 - 10 + 10 + 10;
+console.log(math);
