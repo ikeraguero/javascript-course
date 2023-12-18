cd63269c744305e8febbccf9ba0616c812b1d30e
ikeraguero <ikerpires407@gmail.com>
2023-12-18 19:50:50 -0300

    Add Assignment 8

--- Fundamentals-1/assignments.js
+++ Fundamentals-1/assignments.js
@@ -92,7 +92,7 @@ is this happening?
 7. Finally, convert 'numNeighbours' to a number, and watch what happens now
 when you input 1
 8. Reflect on why we should use the === operator and type conversion in this
-situation */
+situation 
 
 const numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
 
@@ -102,4 +102,29 @@ if (numNeighbours === 1) {
     console.log("More than 1 border!");
 } else {
     console.log("No borders!");
+}
+
+*/
+
+/* LECTURE: Logical Operators
+1. Comment out the previous code so the prompt doesn't get in the way
+2. Let's say Sarah is looking for a new country to live in. She wants to live in a
+country that speaks english, has less than 50 million people and is not an
+island.
+3. Write an if statement to help Sarah figure out if your country is right for her.
+You will need to write a condition that accounts for all of Sarah's criteria. Take
+your time with this, and check part of the solution if necessary.
+4. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If
+not, log 'Portugal does not meet your criteria :('
+5. Probably your country does not meet all the criteria. So go back and temporarily
+change some variables in order to make the condition true (unless you live in
+Canada :D) */
+
+const speaksEnglish = false;
+const hasLessThan50Million = false;
+
+if (speaksEnglish && hasLessThan50Million && !isIsland) {
+    console.log(`You should live in ${country}!`);
+} else {
+    console.log(`${country} does not meet your criteria`)
 }
