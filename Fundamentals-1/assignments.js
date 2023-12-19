bc223ac58ebfffad5786a3cbc75627775dcc52f5
ikeraguero <ikerpires407@gmail.com>
2023-12-18 22:30:14 -0300

    Add Assignment 9 Break

--- Fundamentals-1/assignments.js
+++ Fundamentals-1/assignments.js
@@ -150,11 +150,15 @@ switch (language) {
         break
     case 'english':
         console.log("3rd place and universal language!");
+        break
     case 'hindi':
         console.log("Number 4");
+        break
     case 'arabic':
         console.log("5th most spoken language!")
+        break
     default: {
         console.log("Great language too! :D")
+        break
     }
 }
