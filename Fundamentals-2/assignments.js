82212ced5cd8ed42fbca5dc06ff2d3007f53b76d
ikeraguero <ikerpires407@gmail.com>
2023-12-20 22:21:44 -0300

    Add Assignment 17

--- Fundamentals-2/assignments.js
+++ Fundamentals-2/assignments.js
@@ -120,3 +120,16 @@ const myCountry = {
     population: '214',
     neighbours: ['Uruguay', 'Argentina', 'Paraguay', 'Bolivia', 'Peru', 'Colombia', 'Venezuela', 'Guyana', 'Suriname', 'French Guiana']
 }
+
+/* 1. Using the object from the previous assignment, log a string like this to the
+console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
+and a capital called Helsinki.'
+2. Increase the country's population by two million using dot notation, and then
+decrease it by two million using brackets notation */
+
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbours and a capital called ${myCountry.capital}.`)
+
+myCountry.population = Number(myCountry.population) + 2;
+console.log(myCountry.population);
+myCountry['population'] = Number(myCountry['population']) - 2;
+console.log(myCountry['population'])
--- Fundamentals-2/script.js
+++ Fundamentals-2/script.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const Jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    friends: ['Michael', 'Lewis', 'Max'],
+}
+
+console.log(`${Jonas.firstName} has ${Jonas.friends.length} friends, and his best friend is called ${Jonas.friends[0]}`)
