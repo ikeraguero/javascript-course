dc70c1672c60efc568c6b03818234e9124b302eb
ikeraguero <ikerpires407@gmail.com>
2023-12-21 23:18:26 -0300

    Add Assignment 22

--- Fundamentals-2/assignments.js
+++ Fundamentals-2/assignments.js
@@ -200,3 +200,19 @@ for (let i = 0; i < listOfNeighbours.length; i++) {
         console.log(`Neighbour: ${listOfNeighbours[i][j]}`)
     }
 }
+
+/* LECTURE: The while Loop
+1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing',
+but this time using a while loop (call the array 'percentages3')
+2. Reflect on what solution you like better for this task: the for loop or the while
+loop? */
+
+const percentages3 = [];
+
+let i = 0;
+while (i < populations.length) {
+    percentages3[i] = percentageOfWorld1(populations[i])
+    i++
+}
+
+console.log(percentages2, percentages3)
