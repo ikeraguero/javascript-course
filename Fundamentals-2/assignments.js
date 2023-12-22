31cedbd6c19d075bfed883929107d2d51d6e7374
ikeraguero <ikerpires407@gmail.com>
2023-12-21 22:33:20 -0300

    Add Assignment 20

--- Fundamentals-2/assignments.js
+++ Fundamentals-2/assignments.js
@@ -164,3 +164,24 @@ the console (for numbers 1 to 50): 'Voter number 1 is currently voting' */
 for (let i = 1; i <= 30; i++) {
     console.log(`Voter number ${i} is currently voting.`)
 }
+
+/* LECTURE: Looping Arrays, Breaking and Continuing
+1. Let's bring back the 'populations' array from a previous assignment
+2. Use a for loop to compute an array called 'percentages2' containing the
+percentages of the world population for the 4 population values. Use the
+function 'percentageOfWorld1' that you created earlier
+3. Confirm that 'percentages2' contains exactly the same values as the
+'percentages' array that we created manually in the previous assignment,
+and reflect on how much better this solution is 
+
+const populations = [38, 2, 6, 11] */
+
+const percentages2 = []
+
+for (let i = 0; i < populations.length; i++) {
+    percentages2[i] = percentageOfWorld1(populations[i])
+}
+
+console.log(percentages2);
+console.log(percentages);
+
