14108242bf91e8925797715e23d64b8c391a1bea
ikeraguero <ikerpires407@gmail.com>
2023-12-21 21:00:51 -0300

    Add Assignment 18

--- Fundamentals-2/assignments.js
+++ Fundamentals-2/assignments.js
@@ -113,23 +113,45 @@ saudiArabiaNeighbours[2] = 'Sultanate of Oman';
 properties 'country', 'capital', 'language', 'population' and
 'neighbours' (an array like we used in previous assignments) */
 
-const myCountry = {
-    country: 'Brazil',
-    capital: 'Brasilia',
-    language: 'Portuguese',
-    population: '214',
-    neighbours: ['Uruguay', 'Argentina', 'Paraguay', 'Bolivia', 'Peru', 'Colombia', 'Venezuela', 'Guyana', 'Suriname', 'French Guiana']
-}
+
 
 /* 1. Using the object from the previous assignment, log a string like this to the
 console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
 and a capital called Helsinki.'
 2. Increase the country's population by two million using dot notation, and then
-decrease it by two million using brackets notation */
+decrease it by two million using brackets notation 
 
 console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbours and a capital called ${myCountry.capital}.`)
 
 myCountry.population = Number(myCountry.population) + 2;
 console.log(myCountry.population);
 myCountry['population'] = Number(myCountry['population']) - 2;
-console.log(myCountry['population'])
+console.log(myCountry['population'])*/
+
+/*  LECTURE: Object Methods
+1. Add a method called 'describe' to the 'myCountry' object. This method
+will log a string to the console, similar to the string logged in the previous
+assignment, but this time using the 'this' keyword.
+2. Call the 'describe' method
+3. Add a method called 'checkIsland' to the 'myCountry' object. This
+method will set a new property on the object, called 'isIsland'.
+'isIsland' will be true if there are no neighbouring countries, and false if
+there are. Use the ternary operator to set the property */
+
+const myCountry = {
+    country: 'Brazil',
+    capital: 'Brasilia',
+    language: 'Portuguese',
+    population: '214',
+    neighbours: ['Uruguay', 'Argentina', 'Paraguay', 'Bolivia', 'Peru', 'Colombia', 'Venezuela', 'Guyana', 'Suriname', 'French Guiana'],
+
+    describe: function () {
+        return console.log(`${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbours and a capital called ${this.capital}.`)
+    },
+
+    checkIsland: function () {
+        this.isIsland = this.neighbours.length === 0 ? true : false;
+    }
+}
+
+myCountry.describe();
--- Fundamentals-2/script.js
+++ Fundamentals-2/script.js
@@ -4,6 +4,21 @@ const Jonas = {
     firstName: 'Jonas',
     lastName: 'Schmedtmann',
     friends: ['Michael', 'Lewis', 'Max'],
+    birthYear: 1991,
+    hasDriversLicense: true,
+    job: 'teacher',
+
+    calcAge: function () {
+        this.age = 2023 - this.birthYear;
+        return this.age
+    },
+
+    getSummary: function () {
+        return (`${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense === true ? 'a' : 'no'} driver's license`)
+    }
 }
 
-console.log(`${Jonas.firstName} has ${Jonas.friends.length} friends, and his best friend is called ${Jonas.friends[0]}`)
+console.log(Jonas.calcAge());
+console.log(Jonas.getSummary());
+
+
