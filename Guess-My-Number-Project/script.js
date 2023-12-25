f9325bac1f4e0cabdbfdad241a62036ae40581db
ikeraguero <ikerpires407@gmail.com>
2023-12-25 18:10:34 -0300

    Implementing first scenerio - no number

--- Guess-My-Number-Project/script.js
+++ Guess-My-Number-Project/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* Selecting and manipulating HTML elements - DOM */
+/* Selecting and manipulating HTML elements - DOM 
 
 console.log(document.querySelector('.message').textContent);
 
@@ -10,3 +10,18 @@ document.querySelector('.number').textContent = 10;
 document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
+
+*/
+
+/* Creating the function that JavaScript is going to call once the event of Click happens. */
+const x = function () {
+  /* Selecting the input value and storing it in a variable */
+  const guess = Number(document.querySelector('.guess').value);
+
+  /* Check if there are any number stored in the variable */
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
+};
+
+document.querySelector('.check').addEventListener('click', x);
