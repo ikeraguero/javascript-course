3c3864183d5f6f52f05b284b5cfe6e64800fa2f9
ikeraguero <ikerpires407@gmail.com>
2023-12-25 23:17:21 -0300

    Add Final Game Manipulations

--- Guess-My-Number-Project/script.js
+++ Guess-My-Number-Project/script.js
@@ -14,15 +14,24 @@ document.querySelector('.guess').value = 23;
 */
 
 /*Creating a random number in the beginning of the game */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 // Defining score and highscore variables
 let score = Number(document.querySelector('.score').textContent);
 let highscore = Number(document.querySelector('.highscore').textContent);
 
-document.querySelector('.number').textContent = secretNumber;
+// Creating the play again function
+const againFunction = function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+};
 
 // Creating the function that JavaScript is going to call once the event of Click happens.
-const x = function () {
+const guessFunction = function () {
   // Selecting the input value and storing it in a variable
   const guess = Number(document.querySelector('.guess').value);
 
@@ -33,10 +42,15 @@ const x = function () {
   // Correct value
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct answer';
-    highscore = highscore + score;
     document.querySelector('.highscore').textContent = highscore;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (secretNumber > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // Value too high
   } else if (guess > secretNumber) {
@@ -63,4 +77,8 @@ const x = function () {
 };
 
 /* Selecting the button and defining what's going to happen when it's clicked by passing the function defined above as the second argument*/
-document.querySelector('.check').addEventListener('click', x);
+document.querySelector('.check').addEventListener('click', guessFunction);
+
+// Play again when click 'Again' button
+
+document.querySelector('.again').addEventListener('click', againFunction);
