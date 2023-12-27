8d103ca5ceaf9085d882d0d5e543a58cc36f09a5
ikeraguero <ikerpires407@gmail.com>
2023-12-27 19:44:37 -0300

    Fix Score Logic

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -2,13 +2,15 @@
 
 // Initial game conditions and difining variables
 
-let score1 = (document.getElementById('score--0').textContent = 0);
-let score2 = (document.getElementById('score--1').textContent = 0);
+let score0 = (document.getElementById('score--0').textContent = 0);
+let score1 = (document.getElementById('score--1').textContent = 0);
 
 const player1 = document.querySelector('.player--0');
 const player2 = document.querySelector('.player--1');
 const dice = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
+let currentScore0 = Number(document.getElementById('current--0').textContent);
+let currentScore1 = Number(document.getElementById('current--1').textContent);
 
 // User rolls dice
 
@@ -20,22 +22,22 @@ btnRoll.addEventListener('click', function () {
   // If dice roll number isn't 1
   if (player1.classList.contains('player--active')) {
     if (randomDiceRoll !== 1) {
-      score1 = score1 + randomDiceRoll;
-      document.getElementById('score--0').textContent = score1;
+      currentScore0 = currentScore0 + randomDiceRoll;
+      document.getElementById('current--0').textContent = currentScore0;
     } else if (randomDiceRoll === 1) {
-      score1 = 0;
-      document.getElementById('score--0').textContent = score1;
+      currentScore0 = 0;
+      document.getElementById('current--0').textContent = currentScore0;
       player1.classList.remove('player--active');
       player2.classList.add('player--active');
     }
   } else if (player2.classList.contains('player--active')) {
     if (randomDiceRoll !== 1) {
-      score2 = score2 + randomDiceRoll;
-      document.getElementById('score--1').textContent = score2;
-      console.log(score2);
+      currentScore1 = currentScore1 + randomDiceRoll;
+      document.getElementById('current--1').textContent = currentScore1;
+      console.log(currentScore1);
     } else if (randomDiceRoll === 1) {
-      score2 = 0;
-      document.getElementById('score--1').textContent = score2;
+      currentScore1 = 0;
+      document.getElementById('current--1').textContent = currentScore1;
       player2.classList.remove('player--active');
       player1.classList.add('player--active');
     }
