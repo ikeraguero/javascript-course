42f830319796b94dea3df88ec0b1299f88b6716a
ikeraguero <ikerpires407@gmail.com>
2024-01-07 11:32:38 -0300

    For OF Loop and Enhanced Object Literals

--- 08 - Data-Structures-Operators/script.js
+++ 08 - Data-Structures-Operators/script.js
@@ -1,25 +1,29 @@
 'use strict';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0,
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0,
-      close: 24,
-    },
-  },
+  openingHours,
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -44,6 +48,16 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+// FOR OF LOOP
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1} - ${el}`);
+}
+
+/*
 
 const rest1 = {
   name: 'Capri',
