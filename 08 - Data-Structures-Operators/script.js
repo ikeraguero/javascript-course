3cc6f5967610d6b35490f6c463d9018fcc0720da
ikeraguero <ikerpires407@gmail.com>
2024-01-07 12:31:44 -0300

    Looping Objects (keys, values and entries)

--- 08 - Data-Structures-Operators/script.js
+++ 08 - Data-Structures-Operators/script.js
@@ -49,6 +49,29 @@ const restaurant = {
   },
 };
 
+// LOOPING OBJETCS: KEYS, VALUES AND ENTRIES
+
+// Properties of the object
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+// Property values
+
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+// [key, value]
+
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+
 /*
 
 // OPTIONAL CHAINING - checking if previous element exist before executing something
@@ -170,6 +193,10 @@ restaurant.orderPizza('Mushroom', 'Onion', 'Spinach', 'Olives');
 restaurant.orderPizza('Mushroom');
 
 //Short-circuiting definition: OR operator returns the first truthy value in the operation and the AND operator return first falsy value
+
+&& - if first operant is true, it will return whatever the second operant is
+|| - if first operant is false, it will return whatever the second operant is
+
 
 console.log(undefined || 'Iker');
 console.log(undefined && 'Iker');
