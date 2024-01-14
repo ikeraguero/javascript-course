afcd70e3734975e28814cf918753b26ea6fa13c4
ikeraguero <ikerpires407@gmail.com>
2024-01-14 12:04:14 -0300

    Add Assignments 9, 10, 11 and 12

--- 08 - Data-Structures-Operators/assignments.js
+++ 08 - Data-Structures-Operators/assignments.js
@@ -471,3 +471,116 @@ expected output
 for (const [i, author] of allAuthors.entries()) {
   console.log(`${i}. ${author}`);
 }
+
+// ENHANCED OBJECT LITERALS
+
+/* 9 Below is the bookData array that contains other arrays. Each inner array consists of the property name (first element), and the value (second element). For example, in ['title', 'Computer Networking: A Top-Down Approach'], 'title' is the property name, and 'Computer Networking: A Top-Down Approach' is meant to be the value assigned to that property name.
+
+Using computed properties, fill the newBook object with the properties and values from the bookData array. The first one is done already */
+
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+// Do the rest
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+
+/* 9.2 Below is the pages variable. Add it as a property of the newBook2 object. Use the shorter way. */
+
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages,
+};
+
+// OPTIONAL CHAINING
+
+/* 10 Write a function called getFirstKeyword that takes the book object as an argument. This function should return the first keyword from the book's keywords property (array) or undefined (if the keywords property doesn't exist). It shouldn't throw an error. Use optional chaining for that. 
+
+getFirstKeyword(book[0]);
+output: "computer science"
+*/
+
+const getFirstKeyword = function (book) {
+  console.log(book.keywords?.[0]);
+};
+
+getFirstKeyword(newBook2);
+
+/* 11 Below is the entries variable that stores an empty array. Use the for-of loop together with the Object.keys() method to loop over the thirdParty.goodreads property (array) of the first book object from the books array. For each key, push a new array that contains that key to the entries array.
+
+In the end, the entries array should be filled with arrays containing keys:
+
+[['rating'], ['ratingsCount'], ['reviewsCount'], ['fiveStartRatingCount'], ['oneStartRatingCount']]
+
+*/
+
+const entries = [];
+
+for (const keys of Object.keys(books[0].thirdParty.goodreads)) {
+  entries.push([keys]);
+}
+
+console.log(entries);
+
+/* 11.2 Use the for-of loop together with the Object.values() method and Array's entries() method to loop over thirdParty.goodreads property of the first book from the books array.
+
+Push each value to the appropriate inner array in the entries array (use index from entries()).
+*/
+
+for (const [i, values] of Object.values(
+  books[0].thirdParty.goodreads
+).entries()) {
+  console.log(i, values);
+  entries[i].push(values);
+}
+
+/* 11.3 Use the Object.entries() method on the thirdParty.goodreads property of the first book from the books array. Assign the returned value to the variable called entries2. */
+
+const entries2 = Object.entries(books[0].thirdParty.goodreads);
+
+/* 11.4 Log the entries and entries2 variables to the console, and compare them. They should look the same. */
+console.log(entries);
+console.log(entries2);
+
+// SETS
+
+/* 12 Below is the allKeywords variable, which stores an empty array. Loop over the books array, and fill the allKeywords array with the keywords coming from the keywords property of each book object. The allKeywords array should have just one level (no nested arrays).
+
+Use whatever loop and methods you want. You can also use the spread syntax. In the end, the allKeywords array should look more or less like this: ['computer science', 'programming', 'algorithms', 'data structures', ...]. */
+const allKeywords = [];
+
+for (const book of books) {
+  allKeywords.push(...book.keywords);
+}
+
+/* 12.2 The allKeyword array contains duplicates. Remove them by creating a Set out of that array. Assign the newly created set to the uniqueKeywords variable. */
+
+const uniqueKeywords = new Set(allKeywords);
+console.log(uniqueKeywords);
+
+/* 12.3 Add two more keywords to the uniqueKeywords set, for example, 'coding' and 'science'. */
+
+uniqueKeywords.add('coding').add('science');
+console.log(uniqueKeywords);
+
+/* 12.4 Delete 'business' from the uniqueKeywords set. */
+
+uniqueKeywords.delete('business');
+
+/* 12.5 Create an array out of the uniqueKeywords set, and assign it to the uniqueKeywordsArr variable. */
+
+const uniqueKeywordsArr = [...uniqueKeywords];
+console.log(uniqueKeywordsArr);
+
+/* 12.6 Delete all items from the uniqueKeywords set. */
+
+uniqueKeywords.clear();
