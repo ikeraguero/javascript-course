0a3ba50206973e670d6afc7a8e5bb7198b5943c4
ikeraguero <ikerpires407@gmail.com>
2024-01-27 19:44:09 -0300

    Add Flat and flatMap methods

--- 10 - Arrays-Bankist/script.js
+++ 10 - Arrays-Bankist/script.js
@@ -251,6 +251,39 @@ const displayMovements = function (acc) {
 
 /////////////////////////////////////////////////
 
+// FLAT METHOD
+
+const arr = [[1, 2, 3], [4, 5], 6, 7];
+console.log(arr.flat());
+
+const arrDeep = [1, [2, [3]], [4, [5, 6, [7]]]];
+console.log(arrDeep.flat(3));
+
+// FLATMAP METHOD
+
+// with flat
+const overalBalance = accounts
+  .map(function (acc) {
+    return acc.movements;
+  })
+  .flat()
+  .reduce(function (ac, cur) {
+    return ac + cur;
+  });
+
+console.log(overalBalance);
+
+// with flatMap
+const overalBalance2 = accounts
+  .flatMap(function (acc) {
+    return acc.movements;
+  })
+  .reduce(function (ac, cur) {
+    return ac + cur;
+  });
+
+console.log(overalBalance2);
+
 // REDUCE METHOD
 
 const calcDisplayCurrentBalance = function (acc) {
