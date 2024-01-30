7b3b1f4e71863c2572594fc7b98e11b3e5981f9a
ikeraguero <ikerpires407@gmail.com>
2024-01-29 21:31:25 -0300

    Add Math and Rounding

--- 11 - Numbers-Date -Types-Bankist/script.js
+++ 11 - Numbers-Date -Types-Bankist/script.js
@@ -284,6 +284,31 @@ const calcDisplayCurrentBalance = function (acc) {
 
 // LECTURES
 
+// MATH AND ROUNDING
+
+console.log(Math.sqrt(25));
+console.log(Math.max(5, 18, 23, 11, 2)); // 23
+console.log(Math.min(5, 18, 23, 11, 2)); // 2
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+// General function to generate random numbers
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) * 1) + min;
+
+// Rouding integers
+
+console.log(Math.round(23.3));
+console.log(Math.ceil(23.3)); // 24
+console.log(Math.floor(23.3)); // 23
+console.log(Math.trunc(23.3)); // 23
+
+// Rouding decimals
+
+console.log((2.7).toFixed(0)); // 2
+console.log((2.7).toFixed(3)); // 2.700
+
 // CONVERTING AND CHECKING NUMBERS
 
 // Converting
