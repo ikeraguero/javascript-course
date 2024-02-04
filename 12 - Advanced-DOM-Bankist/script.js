4d8f0f7071df7e49a94c41e0adbb0d6b71e7af3b
ikeraguero <ikerpires407@gmail.com>
2024-02-04 00:35:26 -0300

    Add Insert, Create and Delete

--- 12 - Advanced-DOM-Bankist/script.js
+++ 12 - Advanced-DOM-Bankist/script.js
@@ -28,3 +28,24 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+// Select elements
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+const allButtons = document.getElementsByTagName("button");
+
+// Create and insert elements
+//. insertAdjacentHTML
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.innerHTML =
+  'We use cookies to improve security and user experience! <button class="btn btn--close-cookie">Got it!</button> ';
+header.append(message);
+
+// Delete elements
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
