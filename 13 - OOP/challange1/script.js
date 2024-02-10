c529938e587f4f7d1d0c298cd2d386b505b928d8
ikeraguero <ikerpires407@gmail.com>
2024-02-10 19:14:03 -0300

    Add Challange 1

--- 13 - OOP/challange1/script.js
+++ 13 - OOP/challange1/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* Coding Challenge #1
-Your tasks:
+
 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
 'speed' property. The 'speed' property is the current speed of the car in
 km/h
@@ -18,3 +18,44 @@ the new speed to the console
 Test data:
 § Data car 1: 'BMW' going at 120 km/h
 § Data car 2: 'Mercedes' going at 95 km/h */
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const currentSpeed = function (speed, car) {
+  console.log(`Current ${car} speed is ${speed}`);
+};
+
+Car.prototype.brake = function () {
+  if (this.speed >= 5) {
+    this.speed -= 5;
+  } else {
+    console.log(
+      "Cannot reduce 5 from the current speed, otherwise the car will stop completely"
+    );
+  }
+  return currentSpeed(this.speed, this.make);
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  return currentSpeed(this.speed, this.make);
+};
+
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
+
+bmw.brake();
+bmw.brake();
+bmw.brake();
+bmw.brake();
+bmw.accelerate();
+bmw.accelerate();
+bmw.accelerate();
+
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.brake();
