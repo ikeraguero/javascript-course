b84d1ed859433ef338a8ba6a5f48c5bbb571e052
ikeraguero <ikerpires407@gmail.com>
2024-02-11 19:20:57 -0300

    Add ES6 Classes and Object.create Inheritance

--- 13 - OOP/script.js
+++ 13 - OOP/script.js
@@ -2,6 +2,94 @@
 
 // INHERITANCE BETWEEN CLASSES
 
+// Object.create
+
+const PersonProto = {
+  calcAge() {
+    const now = new Date();
+    return now.getFullYear() - this.birthYear;
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(
+    `My name is ${
+      this.firstName
+    }, I'm ${this.calcAge()} years old and I study ${this.course}`
+  );
+};
+
+const nico = Object.create(StudentProto);
+console.log(nico);
+nico.init("Nico", 1993, "Design");
+nico.introduce();
+/*
+// ES6 Classes
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    const now = new Date();
+    console.log(now.getFullYear() - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+  get age() {
+    const now = new Date();
+    return now.getFullYear() - this.birthYear;
+  }
+  // it's called when we pass the fullName argument during the creation of the object
+  set fullName(name) {
+    if (name.includes(" ")) {
+      this._fullName = name;
+    } else {
+      console.log("Not a valid full name!");
+    }
+  }
+  // call this using lewis.fullName
+  get fullName() {
+    return this._fullName;
+  }
+  // static method - only work on the class itself, not on the instances
+  static hey() {
+    console.log(`Hey, ${this._fullName}`);
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    super(fullName, birthYear);
+    this.course = course;
+  }
+  introduce() {
+    console.log(
+      `My name is ${this.fullName}, I'm ${this.age} years old and I study ${this.course}`
+    );
+  }
+}
+
+const pierre = new StudentCl("Pierre Gasly", 1993, "Computer Science");
+pierre.introduce();
+pierre.greet();
+/*
 // Constructor functions
 
 const Person = function (firstName, birthYear) {
