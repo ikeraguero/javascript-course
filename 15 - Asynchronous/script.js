adcd9ed5f44b8d257e6ab7beac6bab1b22709ccb
ikeraguero <ikerpires407@gmail.com>
2024-02-15 19:14:37 -0300

    Add Section 15 - Requesting Data from Countries API

---
+++ 15 - Asynchronous/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+///////////////////////////////////////
+
+const getCountryCards = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `<article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      data.population / 1000000
+    ).toFixed(1)}M people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+    </article>`;
+
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryCards("belgium");
+getCountryCards("spain");
+getCountryCards("japan");
+getCountryCards("gb");
+getCountryCards("argentina");
+getCountryCards("arabia");
+getCountryCards("nederland");
