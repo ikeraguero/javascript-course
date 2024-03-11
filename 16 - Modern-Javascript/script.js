856010fa5ba13f593e7803ea5a5ff6969a964773
ikeraguero <ikerpires407@gmail.com>
2024-03-11 00:16:55 -0300

    Add Module for Testing

---
+++ 16 - Modern-Javascript/script.js
@@ -0,0 +1,5 @@
+/*import './shoppingCart.js';
+import { addToCart } from './shoppingCart.js'; */
+import add from './shoppingCart.js'
+console.log("Importing")
+add("apples", 21)
---
+++ 16 - Modern-Javascript/shoppingCart.js
@@ -0,0 +1,18 @@
+console.log("Exporting")
+
+const cart = []
+
+export const addToCart = function(product, quantity) {
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} added to cart`);
+}
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export {totalPrice, totalQuantity}
+
+export default  function(product, quantity) {
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} added to cart`);
+}
