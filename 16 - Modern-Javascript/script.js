3125b68dd547e9d16c33eb31e4f82a51552abbaa
ikeraguero <ikerpires407@gmail.com>
2024-03-13 23:48:31 -0300

    Add Note

--- 16 - Modern-Javascript/script.js
+++ 16 - Modern-Javascript/script.js
@@ -3,3 +3,5 @@ import { addToCart } from './shoppingCart.js'; */
 import add from './shoppingCart.js'
 console.log("Importing")
 add("apples", 21)
+
+// Top level use of the await word is only allowed on modules
