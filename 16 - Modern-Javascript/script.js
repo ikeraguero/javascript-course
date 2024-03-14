7dc0bc62df31a56ba63c1812eef1c750f17b8f3a
ikeraguero <ikerpires407@gmail.com>
2024-03-14 13:29:54 -0300

    Add Top Level Await

--- 16 - Modern-Javascript/script.js
+++ 16 - Modern-Javascript/script.js
@@ -6,3 +6,20 @@ add("apples", 21)
 console.log(cart)
 
 // Top level use of the await word is only allowed on modules
+// Top level await will block the execution in the entire module and also where the module is being exported to.
+
+
+const getLastPost = async function() {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json()
+    console.log(data);
+    return {title: data.at(-1).title, text: data.at(-1).body}
+}
+const lastPost = getLastPost();
+console.log(lastPost);
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+
+
