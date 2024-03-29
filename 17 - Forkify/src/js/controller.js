cf6750d3f3f9b75a4aea5bc07382d37d24674d5e
ikeraguero <ikerpires407@gmail.com>
2024-03-29 12:04:38 -0300

    Add getRecipe function

--- 17 - Forkify/src/js/controller.js
+++ 17 - Forkify/src/js/controller.js
@@ -11,3 +11,34 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const getRecipe = async function() {
+  try{
+    const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886")
+    console.log(res);
+
+    const data = await res.json();
+    console.log(data);
+
+    if(!res.ok) throw new Error(`${data.message} (${res.status})`)
+
+    let recipe = data.data.recipe;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      cookingTime: recipe.cooking_time,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+    }
+    console.log(recipe);
+    
+    
+    
+  } catch(err) {
+    alert(err)
+  }
+}
+
+getRecipe()
